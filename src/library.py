38da835d09d4ef54cb22507c8993b70e7a7994a5
lipar2 <sukurmarinko4@gmail.com>
2024-06-13 11:21:57 +0200

    komit

--- src/library.py
+++ src/library.py
@@ -1,7 +1,8 @@
 from book import Book
 
 class Library:
-    def __init__(self):
+    def __init__(self, naziv):
+        self.naziv = naziv
         self.lista_objekata = []
 
     def dodaj_knjigu(self, book):
@@ -9,41 +10,32 @@ class Library:
 
     def ispis_knjiga(self):
         for book in self.lista_objekata:
-            print(book)
+            print(book.display_info())
 
     def search_books(self, search_criteria, keyword):
         results = []
-        with open("fajl.txt", "r") as file:
-            for line in file:
-                book_data = line.strip().split(", ")
-                book = Book(*book_data)
-                if search_criteria.lower() == "naslov" and keyword.lower() in book.naziv.lower():
-                    results.append(book)
-                elif search_criteria.lower() == "autor" and keyword.lower() in book.autor.lower():
-                    results.append(book)
-                elif search_criteria.lower() == "godina" and keyword == book.god_izdanja:
-                    results.append(book)
-                elif search_criteria.lower() == "zanr" and keyword.lower() in book.zanr.lower():
-                    results.append(book)
+        for book in self.lista_objekata:
+            if search_criteria.lower() == "naslov" and keyword.lower() in book.naziv.lower():
+                results.append(book.display_info())
+            elif search_criteria.lower() == "autor" and keyword.lower() in book.autor.lower():
+                results.append(book.display_info())
+            elif search_criteria.lower() == "godina" and keyword == book.god_izdanja:
+                results.append(book.display_info())
+            elif search_criteria.lower() == "zanr" and keyword.lower() in book.zanr.lower():
+                results.append(book.display_info())
         return results
 
     def izmeni_knjigu(self, stari_naziv, novi_naziv, novi_autor, nova_god_izdanja, novi_zanr):
-        with open("fajl.txt", "r") as file:
-            lines = file.readlines()
-
-        with open("fajl.txt", "w") as file:
-            for line in lines:
-                book_data = line.strip().split(", ")
-                if book_data[0] == stari_naziv:
-                    file.write(f"{novi_naziv}, {novi_autor}, {nova_god_izdanja}, {novi_zanr}\n")
-                else:
-                    file.write(line)
+        for book in self.lista_objekata:
+            if book.naziv == stari_naziv:
+                book.naziv = novi_naziv
+                book.autor = novi_autor
+                book.god_izdanja = nova_god_izdanja
+                book.zanr = novi_zanr
+                break
 
     def obrisi_knjigu(self, naziv):
-        with open("fajl.txt", "r") as file:
-            lines = file.readlines()
+        self.lista_objekata = [book for book in self.lista_objekata if book.naziv != naziv]
 
-        with open("fajl.txt", "w") as file:
-            for line in lines:
-                if not line.startswith(naziv):
-                    file.write(line)
+    def broj_knjiga(self):
+        return len(self.lista_objekata)
--- src/main.py
+++ src/main.py
@@ -1,73 +1,112 @@
 from library import Library
 from book import Book
 
-def dodaj_knjigu(library):
-    while True:
-        print("Unesite informacije o knjizi.")
-        naziv = input("Naziv: ")
-        autor = input("Autor: ")
-        god_izdanja = input("Godina izdanja: ")
-        zanr = input("Zanr: ")
-        knjiga = Book(naziv, autor, god_izdanja, zanr)
-        library.dodaj_knjigu(knjiga)
-        prekid = input("Za prekid unesite 'start': ")
-        if prekid == "start":
-            break
+class MenadzerBiblioteka:
+    def __init__(self):
+        self.biblioteke = []
 
-    with open("fajl.txt", "a") as file:
-        for book in library.lista_objekata:
-            file.write(book.display_info() + "\n")
+    def dodaj_biblioteku(self, naziv):
+        nova_biblioteka = Library(naziv)
+        self.biblioteke.append(nova_biblioteka)
 
-def pretrazi_knjige(library):
-    kriterijum = input("Po čemu želite da pretražujete: ")
-    key_word = input("Ključna reč za pretragu: ")
-    results = library.search_books(kriterijum, key_word)
-    print("Rezultati pretrage: ")
-    for result in results:
-        print(result)
+    def obrisi_biblioteku(self, naziv):
+        self.biblioteke = [biblioteka for biblioteka in self.biblioteke if biblioteka.naziv != naziv]
 
-def izmeni_knjigu(library):
-    print("Unesite informacije o knjizi koju želite da izmenite.")
-    stari_naziv = input("Stari naziv: ")
+    def broj_biblioteka(self):
+        return len(self.biblioteke)
 
-    print("Unesite nove informacije o knjizi.")
-    novi_naziv = input("Novi naziv: ")
-    novi_autor = input("Novi autor: ")
-    nova_god_izdanja = input("Nova godina izdanja: ")
-    novi_zanr = input("Novi zanr: ")
+    def dodaj_knjigu(self, naziv_biblioteke):
+        for biblioteka in self.biblioteke:
+            if biblioteka.naziv == naziv_biblioteke:
+                while True:
+                    print("Unesite informacije o knjizi.")
+                    naziv = input("Naziv: ")
+                    autor = input("Autor: ")
+                    god_izdanja = input("Godina izdanja: ")
+                    zanr = input("Zanr: ")
+                    knjiga = Book(naziv, autor, god_izdanja, zanr)
+                    biblioteka.dodaj_knjigu(knjiga)
+                    prekid = input("Za prekid unesite 'start': ")
+                    if prekid == "start":
+                        break
+                break
 
-    library.izmeni_knjigu(stari_naziv, novi_naziv, novi_autor, nova_god_izdanja, novi_zanr)
+    def pretrazi_knjige(self, naziv_biblioteke):
+        for biblioteka in self.biblioteke:
+            if biblioteka.naziv == naziv_biblioteke:
+                kriterijum = input("Po čemu želite da pretražujete: ")
+                key_word = input("Ključna reč za pretragu: ")
+                results = biblioteka.search_books(kriterijum, key_word)
+                print("Rezultati pretrage: ")
+                for result in results:
+                    print(result)
+                break
 
-def prikazi_knjige(library):
-    print("Sve knjige u biblioteci:")
-    library.ispis_knjiga()
+    def izmeni_knjigu(self, naziv_biblioteke):
+        for biblioteka in self.biblioteke:
+            if biblioteka.naziv == naziv_biblioteke:
+                print("Unesite informacije o knjizi koju želite da izmenite.")
+                stari_naziv = input("Stari naziv: ")
 
-def obrisi_knjigu(library):
-    print("Unesite informacije o knjizi koju želite da izbrišete.")
-    naziv = input("Naziv: ")
-    library.obrisi_knjigu(naziv)
+                print("Unesite nove informacije o knjizi.")
+                novi_naziv = input("Novi naziv: ")
+                novi_autor = input("Novi autor: ")
+                nova_god_izdanja = input("Nova godina izdanja: ")
+                novi_zanr = input("Novi zanr: ")
 
-def izlaz(library):
+                biblioteka.izmeni_knjigu(stari_naziv, novi_naziv, novi_autor, nova_god_izdanja, novi_zanr)
+                break
+
+    def prikazi_knjige(self, naziv_biblioteke):
+        for biblioteka in self.biblioteke:
+            if biblioteka.naziv == naziv_biblioteke:
+                print("Sve knjige u biblioteci:")
+                biblioteka.ispis_knjiga()
+                break
+
+    def obrisi_knjigu(self, naziv_biblioteke):
+        for biblioteka in self.biblioteke:
+            if biblioteka.naziv == naziv_biblioteke:
+                print("Unesite informacije o knjizi koju želite da izbrišete.")
+                naziv = input("Naziv: ")
+                biblioteka.obrisi_knjigu(naziv)
+                break
+
+    def broj_knjiga_u_biblioteci(self, naziv_biblioteke):
+        for biblioteka in self.biblioteke:
+            if biblioteka.naziv == naziv_biblioteke:
+                return biblioteka.broj_knjiga()
+
+def izlaz():
     print("Izlaz iz programa.")
     exit()
 
 def main():
-    library = Library()
+    library_manager = MenadzerBiblioteka()
 
     opcije = {
-        "1": dodaj_knjigu,
-        "2": pretrazi_knjige,
-        "3": izmeni_knjigu,
-        "4": prikazi_knjige,
-        "5": obrisi_knjigu,
+        "1": library_manager.dodaj_biblioteku,
+        "2": library_manager.obrisi_biblioteku,
+        "3": lambda: print(f"Broj biblioteka: {library_manager.broj_biblioteka()}"),
+        "4": library_manager.dodaj_knjigu,
+        "5": library_manager.pretrazi_knjige,
+        "6": library_manager.izmeni_knjigu,
+        "7": library_manager.prikazi_knjige,
+        "8": library_manager.obrisi_knjigu,
+        "9": lambda: print(f"Broj knjiga u biblioteci: {library_manager.broj_knjiga_u_biblioteci(input('Naziv biblioteke: '))}"),
         "0": izlaz
     }
 
     while True:
-        unos = input("Šta želite da radite? (1-Dodaj knjigu, 2-Pretraži knjige, 3-Izmeni knjigu, 4-Prikaži sve knjige, 5-Izbriši knjigu, 0-Izlaz): ")
+        print("Opcije: \n1-Dodaj biblioteku \n2-Obriši biblioteku \n3-Broj biblioteka \n4-Dodaj knjigu \n5-Pretraži knjige \n6-Izmeni knjigu \n7-Prikaži sve knjige \n8-Izbriši knjigu \n9-Broj knjiga u biblioteci \n0-Izlaz")
+        unos = input("Šta želite da radite? ")
         opcija = opcije.get(unos)
         if opcija:
-            opcija(library)
+            if unos in ["1", "2", "4", "5", "6", "7", "8"]:
+                naziv = input("Naziv biblioteke: ")
+                opcija(naziv)
+            else:
+                opcija()
         else:
             print("Pogrešan unos, pokušajte ponovo.")
 
